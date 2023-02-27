ea81196d43ea76616ef2fbcfb6073ec4a8f0156e
BarakaIsaac <isaac.mwandau2@gmail.com>
2023-02-27 21:45:16 +0300

    Completed Assignments

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+export default function App() { 
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [dogs, setDogs] = useState([]);
+    
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+          .then((res) => res.json())
+          .then((data) => {
+            setIsLoaded(true);
+            setDogs(data.message);
+          });
+  })
+
+    if (!isLoaded) { 
+        return <p>Loading...</p>
+    } else  {
+        return (
+            <div>
+                <img src={dogs} alt="A Random Dog" />
+            </div>
+        )
+    }
+}
+
+
